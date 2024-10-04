1edfc65e88be6088ad5318979193dc1d6064035c
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-04 19:46:48 +0530

    create model to store images

---
+++ app/Models/GalleryImage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class GalleryImage extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'file_name',
+    ];
+}
